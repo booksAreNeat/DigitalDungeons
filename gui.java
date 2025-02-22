fada3341eeb5390d335300e6c556ed0a846593a2
Abigail Smith <a.smith58@rgu.ac.uk>
2025-02-22 18:01:44 +0000

    Screen switching

--- gui.java
+++ gui.java
@@ -3,32 +3,27 @@ import java.awt.*;
 public class gui extends JFrame {
     CardLayout cardlayout = new CardLayout();
     JPanel mainPanel = new JPanel(cardlayout);
-    JPanel card1 = new JPanel();
-    JPanel card2 = new JPanel();
+    JPanel welcome = new JPanel();
+    JPanel start = new JPanel();
 
-    JButton button1 = new JButton("Go to card 2");
+    JButton button1 = new JButton("Start Game");
     JButton button2 = new JButton("Go to card 1");
 
     gui(){
-        card1.setBackground(Color.red);
-        card2.setBackground(Color.blue);
+        welcome.setBackground(Color.white);
+        start.setBackground(Color.blue);
 
-        card1.add(button1);
-        card2.add(button2);
+        welcome.add(button1);
 
-        mainPanel.add(card1, "card1");
-        mainPanel.add(card2, "card2");
+        mainPanel.add(welcome, "welcome");
+        mainPanel.add(start, "start");
 
         button1.addActionListener((e) ->{
-            cardlayout.show(mainPanel, "card2");
-        });
-
-        button2.addActionListener((e) ->{
-            cardlayout.show(mainPanel, "card1");
+            cardlayout.show(mainPanel, "start");
         });
 
         add(mainPanel);
-        setSize(300,300);
+        setSize(1500,1000);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
